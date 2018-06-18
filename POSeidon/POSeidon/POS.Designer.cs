256a3d8b4616b02bc58402d364ee4d07f039dff9
Matt Maxwell <mrmaxwell78@gmail.com>
2018-06-18 11:16:25 -0400

    More framework development

--- POSeidon/POSeidon/POS.Designer.cs
+++ POSeidon/POSeidon/POS.Designer.cs
@@ -28,20 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.tabPanel = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.tabPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabPanel
+            // 
+            this.tabPanel.Controls.Add(this.tabPage1);
+            this.tabPanel.Controls.Add(this.tabPage2);
+            this.tabPanel.Controls.Add(this.tabPage3);
+            this.tabPanel.Controls.Add(this.tabPage4);
+            this.tabPanel.Controls.Add(this.tabPage5);
+            this.tabPanel.Location = new System.Drawing.Point(3, 35);
+            this.tabPanel.Name = "tabPanel";
+            this.tabPanel.SelectedIndex = 0;
+            this.tabPanel.Size = new System.Drawing.Size(759, 504);
+            this.tabPanel.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+            this.tabPanel.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(751, 478);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Sales History";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(751, 478);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Employee List";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(751, 478);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Inventory List";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(751, 478);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Customer List";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(751, 478);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Loyalty Program";
+            this.tabPage5.UseVisualStyleBackColor = true;
             // 
             // frmPOS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(759, 533);
+            this.Controls.Add(this.tabPanel);
             this.Name = "frmPOS";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "POSeidon Point Of Sale";
+            this.tabPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabPanel;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
--- POSeidon/POSeidon/POS.cs
+++ POSeidon/POSeidon/POS.cs
@@ -16,5 +16,6 @@ namespace POSeidon
         {
             InitializeComponent();
         }
+
     }
 }
